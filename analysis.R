95f5d2f186c009de5cb8b29d1e46c023310ee111
orifjon1801 <128310421+orifjon1801@users.noreply.github.com>
2024-07-19 22:49:49 +0500

    Add files via upload

---
+++ analysis.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+load("rdas/murders.rda")
+murders %>% mutate(abb = reorder(abb, rate)) %>%
+  ggplot(aes(abb, rate)) +
+  geom_bar(width = 0.5, stat = "identity", color = "black") +
+  coord_flip()
